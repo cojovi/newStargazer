88c113a988eff3b997815c8d60af283bde680c94
cojovi <31863818+cojovi@users.noreply.github.com>
2024-07-15 19:44:16 -0500

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,8 +19,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/www.cojovi.com"
-              href="https://www.cojovi.com"
+              title="/about.cojovi.com"
+              href="https://about.cojovi.com"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
