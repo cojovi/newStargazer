26890ecf001fe01e51b33ebcb46dc40a6713535b
cojovi <31863818+cojovi@users.noreply.github.com>
2024-09-12 05:22:28 -0500

    fml
    fml

--- app/layout copy.tsx
+++ app/layout copy.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { useEffect } from "react"; // Import useEffect for chatbot script
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -17,22 +16,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Add the chatbot script using useEffect
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://agentivehub.com/production.bundle.min.js";
-    script.async = true;
-    script.onload = () => {
-      if (window.myChatWidget) {
-        window.myChatWidget.load({
-          assistantId: "5616eacc-1125-4faf-8ea5-89da1dca4e41",
-          apiKey: "e8decef3-5def-452f-9525-8c70759bf246",
-        });
-      }
-    };
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
