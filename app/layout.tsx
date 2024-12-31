44dc81e41f412373d64341399f56ce1e919e6686
cojovi <31863818+cojovi@users.noreply.github.com>
2024-12-30 21:50:34 -0600

    remove chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,37 +32,6 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
 
-        {/* Direct script injection for the chatbot */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d, t) {
-                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-                v.onload = function() {
-                  if (!document.getElementById('root')) {
-                    var root = d.createElement('div');
-                    root.id = 'root';
-                    root.style.position = 'fixed';  
-                    root.style.bottom = '20px';     
-                    root.style.right = '20px';      
-                    root.style.zIndex = '9999';     
-                    d.body.appendChild(root);
-                  }
-                  if (window.myChatWidget && typeof window.myChatWidget.load === 'function') {
-                    window.myChatWidget.load({
-                      assistantId: '5616eacc-1125-4faf-8ea5-89da1dca4e41',
-                      apiKey: 'e8decef3-5def-452f-9525-8c70759bf246',
-                    });
-                  }
-                };
-                v.src = "https://agentivehub.com/production.bundle.min.js";
-                v.type = "text/javascript";
-                s.parentNode.insertBefore(v, s);
-              })(document, 'script');
-            `,
-          }}
-        />
       </body>
     </>
   );
